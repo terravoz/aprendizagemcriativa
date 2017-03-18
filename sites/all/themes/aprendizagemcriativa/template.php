e56a3da03da2ed80722260406e0673adf4220f96
Tamer Zoubi <tamerzg@gmail.com>
2017-03-18 13:01:09 +0100

    i18n revisions projects per author

--- sites/all/themes/aprendizagemcriativa/template.php
+++ sites/all/themes/aprendizagemcriativa/template.php
@@ -28,21 +28,39 @@ function aprendizagemcriativa_preprocess_node(&$variables) {
         break;
       }
     }
+
+    //Link Author to projects/{author_email} view
+    $authors = field_get_items('node', $node, 'field_authors');
+    if($authors) {
+      foreach ($authors as $key => $item) {
+        $author = $item['entity'];
+        $author_email_field = field_get_items('node', $author, 'field_email');
+        $author_email = $author_email_field[0]['email'];
+
+        $institution = field_get_items('node', $author, 'field_institution');
+        $authors_output = $author->title .' ('.$institution[0]['safe_value'].')';
+        $variables['content']['group_basic_info']['field_authors'][$key]['#markup'] = l($authors_output, 'projects/'.$author_email);
+      }
+    }
   }
 
   if($node->type == 'project' && $variables['elements']['#view_mode'] == 'teaser') {
+    //node--project--teaser.tpl.php
     $variables['theme_hook_suggestions'][] = 'node__project__teaser';
+
+    //Author displayed as Name (Institution)
     $authors = field_get_items('node', $node, 'field_authors');
     $authors_output = array();
 
     if($authors) {
-      foreach($authors as $author) {
-        $entity = $author['entity'];
-        $institution = field_get_items('node', $entity, 'field_institution');
-        $authors_output[]= $entity->title .' ('.$institution[0]['safe_value'].')';
+      foreach($authors as $key => $item) {
+        $author = $item['entity'];
+        $institution = field_get_items('node', $author, 'field_institution');
+        $authors_output[]= $author->title .' ('.$institution[0]['safe_value'].')';
       }
     }
     $variables['author'] = implode($authors_output, ", ");
+    //Background image
     $image = field_get_items('node', $node, 'field_image');
     if ($image) {
       $variables['background_image'] = file_create_url($image[0]['uri']);
